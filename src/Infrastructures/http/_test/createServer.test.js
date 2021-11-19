4997c6eca19f2b65e50ac672907b7f9680c1c4a0
binokular <sakurakun002@gmail.com>
2021-11-19 10:50:49 +0700

    ayoo bisa

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -41,7 +41,7 @@ describe('HTTP server', () => {
       // Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(200);
-      expect(responseJson.value).toEqual('Hello world!');
+      expect(responseJson.value).toEqual('Hello Dunyaa!');
     });
   });
 
