c078a5f68b8ae855d65f878cbb6ea04d8a721286
binokular <sakurakun002@gmail.com>
2021-11-19 11:19:11 +0700

    ayoo bisaaaaaaaaa

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -25,7 +25,7 @@ const createServer = async (container) => {
     method: 'GET',
     path: '/',
     handler: () => ({
-      value: 'Hello Dunyaa yang penuh fana!',
+      value: 'Hello Dunyaa!',
     }),
   });
 
